abb05a2c7eccf35d712330cc468d2021ad38cf0a
farispasovic <faris.pasovic@stu.ibu.edu.ba>
2025-12-11 00:03:01 +0100

    -

--- backend/index.php
+++ backend/index.php
@@ -1,27 +1,57 @@
 <?php
 require 'vendor/autoload.php';
+require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/Database.php';
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 require_once __DIR__ . '/services/UserService.php';
 require_once __DIR__ . '/services/ProductService.php';
 require_once __DIR__ . '/services/OrderService.php';
 require_once __DIR__ . '/services/OrderItemService.php';
 require_once __DIR__ . '/services/PaymentService.php';
+require_once __DIR__ . '/services/AuthService.php';
+require_once __DIR__ . '/middleware/AuthMiddleware.php';  
+require_once __DIR__ . '/data/roles.php';          
 
 Flight::register('userService', 'UserService');
 Flight::register('productService', 'ProductService');
 Flight::register('orderService', 'OrderService');
 Flight::register('orderItemService', 'OrderItemService');
 Flight::register('paymentService', 'PaymentService');
+Flight::register('auth_service', 'AuthService'); 
+Flight::register('auth_middleware', 'AuthMiddleware'); 
+
+
+Flight::route('/*', function() {
+    $url = Flight::request()->url;
+
+
+    if (
+        strpos($url, '/auth/login') === 0 ||       
+        strpos($url, '/auth/register') === 0 ||       
+        strpos($url, '/docs') === 0  
+    ) {
+        return true;
+    }
+
+    try {
+        $token = Flight::request()->getHeader("Authentication");
+        Flight::auth_middleware()->verifyToken($token);
+        return true;
+
+    } catch (Exception $e) {
+        Flight::halt(401, $e->getMessage());
+    }
+});
 
 require_once __DIR__ . '/routes/UserRoutes.php';
 require_once __DIR__ . '/routes/ProductRoutes.php';
 require_once __DIR__ . '/routes/OrderRoutes.php';
 require_once __DIR__ . '/routes/OrderItemRoutes.php';
 require_once __DIR__ . '/routes/PaymentRoutes.php';
-
-Flight::route('/', function(){
-    echo "Welcome to OnlineClothes API";
-});
+require_once __DIR__ . '/routes/AuthRoutes.php';
 
 Flight::start();
 ?>
