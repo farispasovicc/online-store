41936f4c73e0b67b06074d494d40bf1e197e70eb
farispasovic <faris.pasovic@stu.ibu.edu.ba>
2025-12-28 00:24:02 +0100

    ??

---
+++ backend/public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../database.php';
+
+require_once __DIR__ . '/../index.php';
+?>
