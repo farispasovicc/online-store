ef54b7ed1d8b89c02f72ad7cefb3402e6d561aa2
farispasovic <faris.pasovic@stu.ibu.edu.ba>
2025-12-28 00:38:51 +0100

    a

--- backend/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/../database.php';
-
-require_once __DIR__ . '/../index.php';
-?>
