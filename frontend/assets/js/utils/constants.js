efd2f35c8a98985b9c02f32c76b7eea0e2d5ee24
farispasovic <faris.pasovic@stu.ibu.edu.ba>
2025-12-28 17:18:21 +0100

    .

--- frontend/assets/js/utils/constants.js
+++ frontend/assets/js/utils/constants.js
@@ -1,5 +1,7 @@
 var Constants = {
-  PROJECT_BASE_URL: "http://localhost/xampp/FarisPasovic/onlineclothesclothes/backend/",
+  PROJECT_BASE_URL: window.location.hostname.includes("ondigitalocean.app")
+    ? "https://stingray-app-hr7pv.ondigitalocean.app/"
+    : "http://localhost/xampp/FarisPasovic/onlineclothesclothes/backend/",
   TOKEN_KEY: "user_token",
   ROLE_ADMIN: "admin",
   ROLE_USER: "user"
